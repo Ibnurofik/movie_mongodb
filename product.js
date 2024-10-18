2a38296b41398c29bff1bdb1544b6dece5a46932
Ibnu Rofik <ibnurofik313@gmail.com>
2024-10-18 23:38:25 +0700

    Membuat schema dengan berbagi jenis data

--- product.js
+++ product.js
@@ -9,29 +9,79 @@ mongoose
     console.log(err);
   });
 
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-  },
-});
+// const productSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true,
+//   },
+//   price: {
+//     type: Number,
+//   },
+// });
 
 const Product = mongoose.model("Product", productSchema);
 
-const tshirt = new Product({
-  name: "T Shirt Raglan",
-  price: 50000,
-  color: "hitam",
-});
+// const tshirt = new Product({
+//   name: "T Shirt Raglan",
+//   price: 50000,
+//   color: "hitam",
+// });
 
-tshirt
-  .save()
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// tshirt
+//   .save()
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+const productSchema = mongoose.Schema({
+  name: {
+    type: string,
+    required: true,
+  },
+  brand: {
+    type: string,
+    required: true,
+  },
+  price: {
+    type: number,
+    required: true,
+    min: 0,
+  },
+  color: {
+    type: string,
+    required: true,
+  },
+  size: [
+    {
+      type: string,
+      required: true,
+    },
+  ],
+  description: {
+    type: string,
+    required: true,
+    maxLength: 150,
+  },
+  condition: {
+    type: string,
+    enum: ["baru", "bekas"],
+    defaul: "baru",
+  },
+  stock: {
+    type: number,
+    required: true,
+    min: 0,
+  },
+  availability: {
+    online: {
+      type: bolean,
+      required: true,
+    },
+    offline: {
+      type: bolean,
+      required: true,
+    },
+  },
+});
